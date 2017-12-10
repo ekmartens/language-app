e7f44fac0f558918eae3d60d91f383a09c28e287
ekmartens <ekmrtns@gmail.com>
2017-12-10 00:38:58 -0800

    fixing table values

--- backend.py
+++ backend.py
@@ -4,6 +4,6 @@ class Database:
     def __init__(self,db):
         self.conn=sqlite3.connect(db)
         self.cur=self.conn.cursor()
-        self.cur.execute('CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, word text, pronunciation text, definition text)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, english text, word text, pronunciation text, definition text)')
         self.cur.execute('CREATE TABLE IF NOT EXISTS rules (id INTEGER PRIMARY KEY, name text, rule text)')
         self.conn.commit()
