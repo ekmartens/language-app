0e67c2632ad350d382a519f0d45c92af4e9544e4
ekmartens <ekmrtns@gmail.com>
2017-12-10 12:29:50 -0800

    finish backend & start frontend connections

--- backend.py
+++ backend.py
@@ -4,6 +4,62 @@ class Database:
     def __init__(self,db):
         self.conn=sqlite3.connect(db)
         self.cur=self.conn.cursor()
-        self.cur.execute('CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, english text, word text, pronunciation text, definition text)')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS rules (id INTEGER PRIMARY KEY, name text, rule text)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, language text, english text, word text, pronunciation text, definition text)')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS rules (id INTEGER PRIMARY KEY, language text, name text, rule text)')
         self.conn.commit()
+
+    def insert_word(self,language,english,word,pronounciation,definition):
+        self.cur.execute('INSERT INTO words VALUES (NULL,?,?,?,?,?)',(language,english,word,pronounciation,definition))
+        self.conn.commit()
+
+    def insert_rule(self,language,name,rule):
+        self.cur.execute('INSERT INTO rules VALUES (NULL,?,?,?)',(language,name,rule))
+        self.conn.commit()
+
+    def view_all_words(self):
+        self.cur.execute('SELECT * FROM words')
+        rows=self.cur.fetchall()
+        return rows
+
+    def view_all_rules(self):
+        self.cur.execute('SELECT * FROM rules')
+        rows=self.cur.fetchall()
+        return rows
+
+    def search_words(self,language='',english='',word='',pronunciation='',definition=''):
+        self.cur.execute('SELECT * FROM words WHERE language=? OR english=? OR word=? OR pronunciation=? OR definition=?', (language,english,word,pronunciation,definition))
+        rows=self.cur.fetchall()
+        return rows
+
+    def search_rules(self,language='',name='',rule=''):
+        self.cur.execute('SELECT * FROM rules WHERE language=? OR name=? OR rule=?', (language,name,rule))
+        rows=self.cur.fetchall()
+        return rows
+
+    def delete_word(self,id):
+        self.cur.execute('DELETE FROM words WHERE id=?',(id,))
+        self.conn.commit()
+
+    def delete_rule(self,id):
+        self.cur.execute('DELETE FROM rules WHERE id=?',(id,))
+        self.conn.commit()
+
+    def update_word(self,id,language,english,word,pronunciation,definition):
+        self.cur.execute('UPDATE words SET language=?, english=?, word=?, pronunciation=?, definition=? WHERE id=?',(language,english,word,pronunciation,definition,id))
+        self.conn.commit()
+
+    def update_rule(self,id,language,name,rule):
+        self.cur.execute('UPDATE rules SET language=?, name=?, rule=? WHERE id=?',(language,name,rule,id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+#language_db = Database("mylanguage.db")
+#language_db.insert_word("Tetzi","Hello","Teldi","TEHL-dee","A peaceful greeting; Love;")
+#language_db.insert_rule("Tetzi","Random Rule","This is my rule.")
+#language_db.delete_rule(2)
+#language_db.update_word(1,"Tetzi","Hello","Teldi","TEHL-dee","A peaceful greeting; Love")
+#language_db.update_rule(1, 'Tetzi', 'Possessive', 'Add “ahí” to the beginning of a word ending in “a”, “o”, or “u” to make it a possessive “my”, for words ending in “i”, add “ahó”')
+#print(language_db.view_all_words())
+#print(language_db.view_all_rules())
--- languageapp.py
+++ languageapp.py
@@ -1,14 +1,45 @@
 from tkinter import *
+from backend import Database
+
+database = Database("mylanguage.db")
+
+def view_words_command():
+    lb1.delete(0,END)
+    for row in database.view_all_words():
+        lb1.insert(END,row)
+
+def view_rules_command():
+    lb2.delete(0,END)
+    for row in database.view_all_rules():
+        lb2.insert(END,row)
+
+def search_words_command():
+    lb1.delete(0,END)
+    for row in database.search_words(lang_text.get(),eng_text.get(),word_text.get(),pronunciation_text.get(),definition_text.get()):
+        lb1.insert(END,row)
+        e_lang.delete(0,END)
+        e1.delete(0,END)
+        e2.delete(0,END)
+        e3.delete(0,END)
+        e4.delete(0,END)
+
+def search_rules_command():
+    lb1.delete(0,END)
+    for row in database.search_rules(lang_text2.get(),name_text.get(),rule_text.get()):
+        lb2.insert(END,row)
+        e_lang2.delete(0,END)
+        e5.delete(0,END)
+        e6.delete(0,END)
 
 window=Tk()
 
 window.wm_title('Fantasy Language Builder')
 
 lb1=Listbox(window,height=8,width=35,bd=3,relief='sunken')
-lb1.grid(row=2,column=0,padx=8)
+lb1.grid(row=2,column=0,padx=8,pady=10)
 
-l1=Label(window,text="DICTIONARY",font=('Courier',18))
-l1.grid(row=0,column=0,pady=20)
+l1=Label(window,text="LANGUAGE",font=('Courier',18))
+l1.grid(row=0,column=0)
 
 l2=Label(window,text="English",font='Courier')
 l2.grid(row=0,column=1)
@@ -22,6 +53,10 @@ l4.grid(row=1,column=1)
 l5=Label(window,text="Definition",font='Courier')
 l5.grid(row=1,column=3)
 
+lang_text=StringVar()
+e_lang=Entry(window,textvariable=lang_text)
+e_lang.grid(row=1,column=0)
+
 eng_text=StringVar()
 e1=Entry(window,textvariable=eng_text)
 e1.grid(row=0,column=2,padx=8)
@@ -30,19 +65,18 @@ word_text=StringVar()
 e2=Entry(window,textvariable=word_text)
 e2.grid(row=0,column=4,padx=8)
 
-pronounciation_text=StringVar()
-e3=Entry(window,textvariable=pronounciation_text)
+pronunciation_text=StringVar()
+e3=Entry(window,textvariable=pronunciation_text)
 e3.grid(row=1,column=2,padx=8)
 
 definition_text=StringVar()
 e4=Entry(window,textvariable=definition_text)
 e4.grid(row=1,column=4,padx=8)
 
-b1=Button(window,text="All",width=10,font='Courier')
-b1.config(relief=SUNKEN)
+b1=Button(window,text="All",width=10,font='Courier',command=view_words_command)
 b1.grid(row=2,column=1,padx=15)
 
-b2=Button(window,text="Search",width=10,font='Courier')
+b2=Button(window,text="Search",width=10,font='Courier',command=search_words_command)
 b2.grid(row=2,column=2,padx=15)
 
 b3=Button(window,text="Add",width=10,font='Courier')
@@ -54,11 +88,11 @@ b4.grid(row=2,column=4,padx=15)
 b5=Button(window,text="Delete",width=10,font='Courier')
 b5.grid(row=4,column=0,pady=12)
 
-l6=Label(window,text="GRAMMAR",font=('Courier',18))
+l6=Label(window,text="GRAMMAR (Enter Language)",font=('Courier',18))
 l6.grid(row=6,column=0)
 
 lb2=Listbox(window,height=8,width=35,bd=3,relief='sunken')
-lb2.grid(row=8,column=0,padx=8)
+lb2.grid(row=8,column=0,padx=8,pady=10)
 
 l7=Label(window,text="Rule Name",font='Courier')
 l7.grid(row=6,column=1)
@@ -66,18 +100,22 @@ l7.grid(row=6,column=1)
 l3=Label(window,text="Rule",font='Courier')
 l3.grid(row=6,column=3)
 
+lang_text2=StringVar()
+e_lang2=Entry(window,textvariable=lang_text2)
+e_lang2.grid(row=7,column=0)
+
 name_text=StringVar()
 e5=Entry(window,textvariable=name_text)
 e5.grid(row=6,column=2,padx=8)
 
-name_text=StringVar()
-e6=Entry(window,textvariable=name_text)
+rule_text=StringVar()
+e6=Entry(window,textvariable=rule_text)
 e6.grid(row=6,column=4,padx=8)
 
-b6=Button(window,text="All",width=10,font='Courier')
+b6=Button(window,text="All",width=10,font='Courier',command=view_rules_command)
 b6.grid(row=8,column=1,padx=15)
 
-b7=Button(window,text="Search",width=10,font='Courier')
+b7=Button(window,text="Search",width=10,font='Courier',command=search_rules_command)
 b7.grid(row=8,column=2,padx=15)
 
 b8=Button(window,text="Add",width=10,font='Courier')
@@ -88,6 +126,9 @@ b9.grid(row=8,column=4,padx=15)
 
 b10=Button(window,text="Delete",width=10,font='Courier')
 b10.grid(row=9,column=0,pady=12)
+
+b11=Button(window,text="Close",width=10,font='Courier')
+b11.grid(row=9,column=4)
 
 #lb1.bind('<<ListboxSelect>>',get_selected_row)
 
